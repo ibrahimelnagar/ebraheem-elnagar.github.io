59aacb3a1f5aa1d36af2ff18a60d2a7180fdf6f9
ibrahimelnagar <ebraheem.elnagar@gmail.com>
2024-12-06 18:36:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // Typed.js for dynamic job titles
     const typedTextElement = document.getElementById('typed-text');
     const jobTitles = [
-        'Software Developer',
-        'Tech Innovator',
-        'Problem Solver',
-        'Creative Technologist'
-    ];
+    'Project & Operations Manager',
+    'District Operation Manager at NATGAS',
+    'Infrastructure Management Expert',
+    'Team Leadership Professional'
+];
 
     new Typed(typedTextElement, {
         strings: jobTitles,
